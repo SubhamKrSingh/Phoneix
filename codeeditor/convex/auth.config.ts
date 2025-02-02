4cfec0be076a8071c34734a5520d08e7588a6b48
Itsmilotic <itsmilotic@gmail.com>
2025-02-03 01:48:52 +0530

    Updated snippets and refactored snippet page

---
+++ codeeditor/convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://promoted-dane-77.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ codeeditor/src/app/snippets/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { useQuery } from "convex/react";
+import { api } from './../../../convex/_generated/api';
+import { useState } from 'react';
+import SnippetsPageSkeleton from './_components/SnippetsPageSkeleton';
+import NavigationHeader from './../../components/NavigationHeader';
+
+function SnippetsPage() {
+    const snippets = useQuery(api.snippets.getSnippets);
+    const [searchQuery, setSearchQuery] = useState("")
+    const [selectedLanguage, setSelectedLanguage] = useState<string | null>(null);
+    const [view, setView] = useState<"grid" | "list">("grid");
+
+    if (snippets === undefined) {
+        return (
+          <div className="min-h-screen">
+            <NavigationHeader />
+            <SnippetsPageSkeleton />
+          </div>
+        );
+    }
+}
+
+
+export default SnippetsPage;
